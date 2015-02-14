3d2d6dec468ccf27b02e7f7781b6c48b01da9d0f
harshad das <harshaddas2010@gmail.com>
2015-02-14 20:46:31 +0530

    teacher

--- app/assets/javascripts/angular/services/factory/teacherFactory.js
+++ app/assets/javascripts/angular/services/factory/teacherFactory.js
@@ -3,6 +3,11 @@ schoolServices.factory('Teacher',['$http',function($http){
 	return {
 		list: function(callback){
 			$http.get('teachers').success(callback);
+		},
+		getTeacher: function(callback,teacherId)
+		{
+			var url = "teachers/"+teacherId;
+			$http.get(url).success(callback);
 		}
 	};
 }]);
